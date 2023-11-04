c4efbd3de429eb698d7abbb050e3d6e62d5f7716
Jkakwenya <93216139+Jkakwenya@users.noreply.github.com>
2023-11-04 13:06:02 -0500

    Added login and Signup

--- smithinnovates/src/frontend/Login.js
+++ smithinnovates/src/frontend/Login.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { collection, addDoc } from "firebase/firestore";
+import { auth, db } from '../backend/firebase'
+import { useNavigate} from 'react-router-dom'
 
 function Login() {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-  const handleLogin = () => {
-    // Implement your login logic here
-    console.log('Logging in with username:', username);
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+        signInWithEmailAndPassword( auth, email, password).then((auth) => {
+            if (auth){
+                navigate('/')
+            }
+        }).catch(error => alert(error.message))
+
   };
 
   return (
@@ -15,9 +26,9 @@ function Login() {
       <div>
         <input
           type="text"
-          placeholder="Username/AA-Advantage number"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Email/AA-Advantage number"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </div>
       <div>
@@ -34,18 +45,44 @@ function Login() {
 }
 
 function Signup() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [newUsername, setNewUsername] = useState('');
-  const [newPassword, setNewPassword] = useState('');
+  const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [state, setState] = useState('');
   const [country, setCountry] = useState('');
+  const navigate = useNavigate();
 
-  const handleSignup = () => {
-    // Implement your signup logic here
-    console.log('Signing up with username:', newUsername);
-    console.log('Email:', email);
-    console.log('State:', state);
-    console.log('Country:', country);
+  const newUser = {
+    firstName: firstName,
+    lastName: lastName,
+    country: country,
+    username: newUsername,
+    email: email,
+    state: state
+  }
+
+
+  const handleSignup = (event) => {
+    event.preventDefault();
+    createUserWithEmailAndPassword( auth, email, password)
+  .then((auth) => {
+    if (auth){
+      const user = auth.user;
+      const userRef = collection(db, `users/${user?.uid}/info`);
+
+      addDoc(userRef, newUser)
+        .then(() => {
+          console.log("Data written to database");
+        })
+        .catch((error) => {
+          console.error("Error writing data to database: ", error);
+        });
+
+      navigate('/')
+    }
+  }).catch(error => alert(error.message));
   };
 
   return (
@@ -53,42 +90,54 @@ function Signup() {
       <h1>Sign Up</h1>
       <input
         type="text"
+        placeholder="FirstName"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="LastName"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <input
+        type="text"
         placeholder="Username"
         value={newUsername}
         onChange={(e) => setNewUsername(e.target.value)}
       />
-      <div>
+      
         <input
           type="password"
           placeholder="Password"
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-      </div>
-      <div>
+      
+      
         <input
           type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-      </div>
-      <div>
+      
+      
         <input
           type="text"
           placeholder="State"
           value={state}
           onChange={(e) => setState(e.target.value)}
         />
-      </div>
-      <div>
+      
+      
         <input
           type="text"
           placeholder="Country"
           value={country}
           onChange={(e) => setCountry(e.target.value)}
         />
-      </div>
+      
       <button onClick={handleSignup}>Sign Up</button>
     </div>
   );
