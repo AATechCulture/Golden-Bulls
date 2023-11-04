e43ccea9a243b7f0cdec573e7df4730b6700bbe0
J'erome Harsant Adonis <adonisherome73@gmail.com>
2023-11-04 14:13:10 -0400

    Basic header display

--- smithinnovates/src/frontend/Header.js
+++ smithinnovates/src/frontend/Header.js
@@ -1,15 +1,24 @@
-import React from 'react'
-import { Link} from 'react-router-dom'
+import React from 'react';
+import './styles/Header.css';
+import { Link } from 'react-router-dom'; // Assuming you're using React Router for navigation
 
 function Header() {
-
   return (
-    <div>
-      <h1>Header</h1>
-      <Link to='/login'><span>Login</span></Link>
-      
-    </div>
-  )
+    <header className="header">
+      <div className="left-section">
+        <div className="hamburger-icon">☰</div>
+        <div className="profile-section">
+          <div className="settings">Settings</div>
+          <div className="book-flight">Book Flight</div>
+          <div className="search">Search</div>
+        </div>
+      </div>
+      <div className="right-section">
+        <Link to="/login" className="login-link">Login</Link>
+        {/* Add any other content or components for the right section */}
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
