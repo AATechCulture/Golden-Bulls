3b506addd1a805319392a3a390488f42bf38e44e
Jkakwenya <93216139+Jkakwenya@users.noreply.github.com>
2023-11-04 11:57:48 -0500

    Added login link

--- smithinnovates/src/App.js
+++ smithinnovates/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom" 
 import Home from './frontend/Home';
 import Header from './frontend/Header';
+import Login from './frontend/Login';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
+      <Route path='/login' element={(<> <Header /><Login /></>)} />
         <Route path='/' element={(<> <Header /><Home /></>)} />
         </Routes></BrowserRouter>
     </div>
--- smithinnovates/src/frontend/Header.js
+++ smithinnovates/src/frontend/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link} from 'react-router-dom'
 
 function Header() {
+
   return (
     <div>
       <h1>Header</h1>
+      <Link to='/login'><span>Login</span></Link>
+      
     </div>
   )
 }
