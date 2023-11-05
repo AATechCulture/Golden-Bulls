ad61c1b3866219cecef5eea11c5853d3b465db25
Jkakwenya <93216139+Jkakwenya@users.noreply.github.com>
2023-11-04 22:07:08 -0500

    Added reminder carousel

--- smithinnovates/src/frontend/ReminderHome.js
+++ smithinnovates/src/frontend/ReminderHome.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import Reminder from './Reminder';
+//import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import './styles/ReminderHome.css'
+import { Carousel } from 'react-responsive-carousel';
 
 function ReminderHome() {
   // Replace with your actual flight time
   const flightTime = new Date('2023-11-09T10:00:00');
 
+  
+
+
   return (
     <div>
       <h1>Welcome to Your Flight Reminder</h1>
-      <div className="fun-images">
-        <img src="airplane-image.jpg" alt="Airplane" />
-        <img src="beach-image.jpg" alt="Beach" />
-        {/* Add more fun images or elements */}
+      <div className="main-carousel-container">
+        <Carousel
+          showThumbs={false}
+          infiniteLoop={true}
+          autoPlay={true}
+          interval={5000}
+        >
+          <div className="carousel-container">
+            <img src="/media/tower.jpg" alt="" />
+          </div>
+          <div className="carousel-container">
+            <img src="/media/tower1.jpg" alt="" />
+          </div>
+          <div className="carousel-container">
+            <img src="/media/inflight.jpg" alt="" />
+          </div>
+          
+        </Carousel>
       </div>
       <h2>Flight Details</h2>
       {/* Display flight information */}
